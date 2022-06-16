ef15437fbf41fbda4d9a6321aa9d8d55780505fa
Phạm Bạch Minh Trí <phambachminhtri9.12@gmail.com>
2022-06-16 16:22:05 +0700

    Updated

---
+++ Tuan5_PhamBachMinhTri/Controllers/GioHangController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan5_PhamBachMinhTri.Models;
+namespace Tuan5_PhamBachMinhTri.Controllers
+{
+    public class GioHangController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> listGiohang = Session["Giohang"] as List<Giohang>; 
+            if(listGiohang == null)
+            {
+                listGiohang = new List<Giohang>();
+                Session["Giohang"] = listGiohang;
+            }
+            return listGiohang;
+        }
+        public ActionResult ThemGioHang(int id, string strURL)
+        {
+
+        }
+    }
+}
