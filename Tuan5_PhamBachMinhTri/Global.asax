104359e0f335171ee21bda6b4b00267f7160b537
Phạm Bạch Minh Trí <phambachminhtri9.12@gmail.com>
2022-06-16 15:43:55 +0700

    Add project files.

---
+++ Tuan5_PhamBachMinhTri/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tuan5_PhamBachMinhTri.MvcApplication" Language="C#" %>
---
+++ Tuan5_PhamBachMinhTri/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tuan5_PhamBachMinhTri.Startup))]
+namespace Tuan5_PhamBachMinhTri
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
